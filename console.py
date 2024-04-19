02b3c8044131aa272cd19f13e3ca436aea3e9f22
emma-obelenge <obeleemmanuel@gmail.com>
2024-04-19 18:19:20 +0100

    modify console for task 2

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -115,16 +115,38 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        if not args:
+        try:
+            class_name = args.split(" ")[0]
+        except IndexError:
+            pass
+        if not class_name:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        elif class_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
+        """
         new_instance = HBNBCommand.classes[args]()
         storage.save()
         print(new_instance.id)
-        storage.save()
+        """
+        full_list = args.split(" ")
+        new_inst = eval(class_name)()
+        for iter in range(1, len(full_list)):
+            key, val = tuple(full_list[iter].split("="))
+            if val.startswith('"'):
+                value = val.strip('"').replace("_", " ")
+            else:
+                try:
+                    value = eval(value)
+                except Exception:
+                    print(f"Evaluaition of {value} failed")
+                    pass
+            if hasattr(new_inst, key):
+                setattr(new_inst, key, value)
+        storage.new(new_inst)
+        print(new_inst.id)
+        new_inst.save()
 
     def help_create(self):
         """ Help information for the create method """
@@ -187,7 +209,7 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            del(storage.all()[key])
+            del (storage.all()[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
@@ -272,7 +294,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +302,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
@@ -319,6 +341,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
--- tests/test_console.py
+++ tests/test_console.py
@@ -7,6 +7,11 @@ import console
 import inspect
 import pep8
 import unittest
+import os
+from unittest.mock import patch
+from models.engine.file_storage import FileStorage
+from console import HBNBCommand
+from io import StringIO
 HBNBCommand = console.HBNBCommand
 
 
@@ -18,6 +23,20 @@ class TestConsoleDocs(unittest.TestCase):
         result = pep8s.check_files(['console.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
+    
+    def test_creat_for_errors(self):
+        """
+        Checks for create command errors. If class name
+        is missing or does'nt exist
+        """
+        with patch("sys.stdout", new=StrigIO()) as f:
+            self.HBNB.onecmd("create")
+            self.assertEqual(
+                "** class name missing **\n", f.getvalue())
+            with patch("sys.stdout", new=StringIO()) as f:
+                self.HBNB.onecmd("create fake_class")
+                self.assertEqual(
+                    "** class doesn't exist **\n", f.getvalue())
 
     def test_pep8_conformance_test_console(self):
         """Test that tests/test_console.py conforms to PEP8."""
@@ -25,6 +44,72 @@ class TestConsoleDocs(unittest.TestCase):
         result = pep8s.check_files(['tests/test_console.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
+
+    def test_create_command_validity(self):
+        """ Would test the create command"""
+        with patch("sys.stdout", newStringIO()) as f:
+            self.HBNB.onecmd("create BaseModel")
+            bmodel = f.getvalue().strip()
+
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.HBNB.onecmd("create User")
+            usr = f.getvalue().strip()
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.HBNB.onecmd("create State")
+            t_state = f.getvalue().strip()
+        
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.HBNB.onecmd("create Place")
+            plc = f.getvalue().strip()
+
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.HBNB.onecmd("create City")
+            citi = f.getvalue().strip()
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.HBNB.onecmd("create Review")
+            rev = f.getvalue().strip()
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.HBNB.onecmd("create Amenity")
+            ameni = f.getvalue().strip()
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.HBNB.onecmd("all Basemodel")
+            self.assertIn(bmodel, f.getvalue())
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.HBNB.onecmd("all User")
+            self.assertIn(usr, f.getvalue())
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.HBNB.onecmd("all State")
+            self.assertIn(t_state, f.getvalue())
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.HBNB.onecmd("all Amenity")
+            self.assertIn(ameni, f.getvalue())
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.HBNB.onecmd("all Place")
+            self.assertIn(plc, f.getvalue())
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.HBNB.onecmd("all City")
+            self.assertIn(citi, f.getvalue())
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.HBNB.onecmd("all Review")
+            self.assertIn(rev, f.getvalue())
+            
+    def test_create_command_with_kwargs(self):
+        """would run check on the create command with kwargs"""
+        with patch("sys.stdout", new=StringIO()) as f:
+            call = (f'create Place city_id="0001" user_id="0001" name="My_little_house" number_rooms=4 number_bathrooms=2 max_guest=10 price_by_night=300 latitude=37.773972 longitude=-122.431297')
+            self.HBNB.onecmd(call)
+            plc = f.getvalue().strip()
+        
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.HBNB.onecmd("all Place")
+            output = f.getvalue()
+            self.assertIn(plc, output)
+            self.assertIn("'city_id': '0001'", output)
+            self.assertIn("'name': 'My house'", output)
+            self.assertIn("'number_rooms: '4'", output)
+            self.assertIn("'latitude': '37.77'", output)
+            self.assertIn("'longitude': '43.434'", output)
+            
 
     def test_console_module_docstring(self):
         """Test for the console.py module docstring"""
