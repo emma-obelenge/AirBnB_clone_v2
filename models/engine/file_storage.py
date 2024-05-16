c62fa3795f1b55a1615e5efd8233cd0c21e21783
emma-obelenge <obeleemmanuel@gmail.com>
2024-05-16 20:14:25 +0100

    update on task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
-
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
@@ -9,22 +15,27 @@ class FileStorage:
     __objects = {}
 
     def delete(self, obj=None):
-        """deletes obj from ___objects if it's inside"""
-        if obj is not None:
-            key = obj.__class__.name__ + '.' + obj.id
-            if key in self.__objects:
-                del self.__objects[key]
-            self.save()
+        """deletes obj from __objects if it is inside"""
+        if obj is None:
+            return
+        obj_to_del = f"(obj.__class__.__name__).(obj.id)"
+        try:
+            FileStorage.__objects[obj_to_del]
+        except AttributeError:
+            pass
+        except KeyboardInterrupt:
+            pass
 
     def all(self, cls=None):
-        """Returns the list of objects of one type of class"""
-        if cls is not None:
-            new_dict = {}
-            for key, value in self.__objects.item():
-                if cls == value.__class__ or cls == value.__class__.__name__:
-                    new_dict[key] = value
-                return new_dict
-        return self.__objects
+        """Returns a dictionary of models currently in storage"""
+        if cls:
+            if isinstance(cls, str):
+                cls = globals().get(cls)
+            if cls and issubclass(cls, BaseModel):
+                cls_dict = {k: v for k,
+                            v in self.__objects.items() if isinstance(v, cls)}
+                return cls_dict
+        return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -59,6 +70,8 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                    self.all()[key] = classes[val['__class__']](**val)
+                        self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
+            pass
+        except json.decoder.JSONDecodeError:
             pass
