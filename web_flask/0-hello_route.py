d544db1f8db7de18852280f78099ae7756e85d3b
Amele06 <kelvina0306@gmail.com>
2024-05-19 19:50:30 +0000

    script that starts a Flask web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application:
+"""
+
+from web_flask import app
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Displays 'Hello HBNB!'
+    Returns:
+        str: "Hello HBNB"
+    """
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
