6022edead5a45571bdb99449e0622fb2b387bb43
emma-obelenge <obeleemmanuel@gmail.com>
2024-06-06 11:26:20 +0100

    handled _sa_instance_state in file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -37,6 +37,9 @@ class FileStorage:
             if cls and issubclass(cls, BaseModel):
                 cls_dict = {k: v for k,
                             v in self.__objects.items() if isinstance(v, cls)}
+                for k, v in cls_dict.items():
+                    if '_sa_instance_state' in v.__dict__:
+                        del v.__dict__['_sa_instance_state']
                 return cls_dict
         return FileStorage.__objects
 
