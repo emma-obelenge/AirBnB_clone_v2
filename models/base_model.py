7542ff7a6e6cf084b4b0c39cc0b05596f38c361a
emma-obelenge <obeleemmanuel@gmail.com>
2024-06-06 12:52:46 +0100

    handled _sa_instance_state in file_storage

--- models/base_model.py
+++ models/base_model.py
@@ -36,7 +36,9 @@ class BaseModel:
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        obj_dict = self.__dict__.copy()
+        obj_dict.pop('_sa_instance_state', None)
+        return '[{}] ({}) {}'.format(cls, self.id, obj_dict)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
